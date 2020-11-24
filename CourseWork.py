062aabdaa641568541907eedc5b718a77f707656
Anastasiya <sp.maa@mail.ru>
2020-11-24 13:06:12 +0300

    Добавила вызов окна Пациент для кнопки Запись в главном окне main_window

--- CourseWork.py
+++ CourseWork.py
@@ -1,17 +1,29 @@
 from PyQt5 import QtWidgets
-from CourseWorkQt import Ui_MainWindow  # импорт нашего сгенерированного файла
+from CourseWorkQt import Ui_MainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton
 from PyQt5.QtCore import pyqtSignal, QObject
+from Patient import Ui_DialogPatientName
 import sys
- 
  
 class mywindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(mywindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-       
-app = QtWidgets.QApplication([])
-application = mywindow()
-application.show()
- 
-sys.exit(app.exec())
+        self.ui.appointButton.clicked.connect(self.show_patient_window)
+        
+    def show_patient_window(self):
+        self.w2 = patientwindow()
+        self.w2.show()
+        
+class patientwindow(QtWidgets.QDialog):
+    def __init__(self):
+        super(patientwindow, self).__init__()
+        self.ui = Ui_DialogPatientName()
+        self.ui.setupUi(self)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = mywindow()
+    w.show()
+    sys.exit(app.exec_())

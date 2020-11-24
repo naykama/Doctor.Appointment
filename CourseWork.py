222df0d8425b56e3328abe9f0ba9bbf2faa98f78
Anastasiya <sp.maa@mail.ru>
2020-11-24 13:23:57 +0300

    Добавила вызов окна Настройки для кнопки Настройки в главном окне main_window

--- CourseWork.py
+++ CourseWork.py
@@ -3,6 +3,7 @@ from CourseWorkQt import Ui_MainWindow
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton
 from PyQt5.QtCore import pyqtSignal, QObject
 from Patient import Ui_DialogPatientName
+from Fix import Ui_DialogFix
 import sys
  
 class mywindow(QtWidgets.QMainWindow):
@@ -11,9 +12,14 @@ class mywindow(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.appointButton.clicked.connect(self.show_patient_window)
-        
+        self.ui.fixButton.clicked.connect(self.show_fix_window)
+       
     def show_patient_window(self):
         self.w2 = patientwindow()
+        self.w2.show()
+
+    def show_fix_window(self):
+        self.w2 = fixwindow()
         self.w2.show()
         
 class patientwindow(QtWidgets.QDialog):
@@ -21,6 +27,13 @@ class patientwindow(QtWidgets.QDialog):
         super(patientwindow, self).__init__()
         self.ui = Ui_DialogPatientName()
         self.ui.setupUi(self)
+
+class fixwindow(QtWidgets.QDialog):
+    def __init__(self):
+        super(fixwindow, self).__init__()
+        self.ui = Ui_DialogFix()
+        self.ui.setupUi(self)
+    
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)

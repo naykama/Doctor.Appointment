6cfe21b92125fb1ae5444ca5168220a451903848
Anastasiya <sp.maa@mail.ru>
2020-11-25 11:57:47 +0300

    Добавила окно Врач с работающей кнопкой Назад

--- CourseWork.py
+++ CourseWork.py
@@ -4,6 +4,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton
 from PyQt5.QtCore import pyqtSignal, QObject
 from Patient import Ui_DialogPatientName
 from Fix import Ui_DialogFix
+from Doctor import Ui_DialogDoctor
 import sys
  
 class mywindow(QtWidgets.QMainWindow):
@@ -27,12 +28,25 @@ class patientwindow(QtWidgets.QDialog):
         super(patientwindow, self).__init__()
         self.ui = Ui_DialogPatientName()
         self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.show_doctor_window)
+
+    def show_doctor_window(self):
+        self.w2 = doctorwindow()
+        self.w2.show()
+        
 
 class fixwindow(QtWidgets.QDialog):
     def __init__(self):
         super(fixwindow, self).__init__()
         self.ui = Ui_DialogFix()
         self.ui.setupUi(self)
+
+class doctorwindow(QtWidgets.QDialog):
+    def __init__(self):
+        super(doctorwindow, self).__init__()
+        self.ui = Ui_DialogDoctor()
+        self.ui.setupUi(self)
+        self.ui.pushButton_2.clicked.connect(self.close)
     
 
 if __name__ == '__main__':
